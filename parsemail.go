cf8e8192ec9baddce04f21db3911bd0fc84f21ce
Xiao Han <justlaputa@gmail.com>
2017-11-14 09:41:00 +0900

    add sample mail, parsemail.go can be a cli tool

---
+++ parsemail.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/jhillyerd/enmime"
+)
+
+func main() {
+	filename := os.Args[1]
+
+	filedata, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("failed to read file, %v", err)
+	}
+
+	message, err := enmime.ReadEnvelope(bytes.NewReader(filedata))
+	if err != nil {
+		log.Fatalf("failed to parse mail, %v", err)
+	}
+
+	log.Printf(message.HTML)
+}
